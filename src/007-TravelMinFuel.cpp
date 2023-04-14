131fe02e83189421e745655389f65baf7f5c2d94
韵 <98337638+CQJTU-Linzd@users.noreply.github.com>
2023-04-14 12:05:09 +0800

    Create 007-TravelMinFuel.cpp

---
+++ src/007-TravelMinFuel.cpp
@@ -0,0 +1,45 @@
+// P7
+// https://www.bilibili.com/video/BV1DT411s7hp?p=7&vd_source=2286e10835cc4cd375b26d18d9e6fa7e
+class TravelMinFuel {
+    int cnt;
+public:
+    // n个居民点，n + 1个节点
+    int minFuel(vector<int>& a, vector<int>& b, int n) {
+        // 先建图 邻接表
+        vector<vector<int>>graph(n + 1);
+        for (int i = 0; i < a.size(); i++) {
+            graph[a[i]].push_back(b[i]);
+            graph[b[i]].push_back(a[i]);
+        }
+        // 建图完毕
+        // 办公室是0号点 所有员工往0号点汇聚
+        
+        // dfn序 
+        // dfn[a] != 0 说明a号点没遍历过
+        vector<int>dfn(n + 1);
+
+        // a为头的树，一共有10个节点
+        // 没遍历的时候，size[a] = 0
+        // 遍历完a，size[a] = 10
+        vector<int>size(n + 1);
+
+        // cost[a]：所有居民汇聚到a，总油量是多少
+        vector<int>cost(n + 1);
+
+        cnt = 0;
+        dfs(graph, 0, dfn, size, cost);
+        return cost[0];
+    }
+    void dfs(vector<vector<int>>& graph, int cur, vector<int>& dfn, vector<int>& size, vector<int>& cost) {
+        dfn[cur] = ++cnt;
+        size[cur] = 1;
+        for (int next : graph[cur]) {
+            if (dfn[next]) { // 只有dfn序是0，才往下走
+                dfs(graph, next, dfn, size, cost);
+                size[cur] += size[next];
+                cost[cur] += cost[next]; // next所有孩子，汇聚到next花了多少
+                cost[cur] += (size[next] + 5 - 1) / 5; // next及其所有孩子，一起汇聚到cur的花费
+            }
+        }
+    }
+};

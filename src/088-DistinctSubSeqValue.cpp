bb1aaadbdb3a04663cc797655b1b8bbac33f7751
韵 <98337638+CQJTU-Linzd@users.noreply.github.com>
2023-06-28 22:02:05 +0800

    Update 088-DistinctSubSeqValue.cpp

--- src/088-DistinctSubSeqValue.cpp
+++ src/088-DistinctSubSeqValue.cpp
@@ -1,5 +1,6 @@
 // 88
 // https://www.bilibili.com/video/BV1DT411s7hp?p=88&vd_source=2286e10835cc4cd375b26d18d9e6fa7e
+// 本题测试链接 : https://leetcode.com/problems/distinct-subsequences-ii/
 class DistinctSubSeqValue {
 public:
 

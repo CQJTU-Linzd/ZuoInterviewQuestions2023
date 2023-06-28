7371b614b4e72ff82b86e74ab6d7ad2e9ebc2214
韵 <98337638+CQJTU-Linzd@users.noreply.github.com>
2023-06-28 21:02:34 +0800

    Update 102-Heaters.cpp

--- src/102-Heaters.cpp
+++ src/102-Heaters.cpp
@@ -1,5 +1,11 @@
 // 102
 // https://www.bilibili.com/video/BV1DT411s7hp?p=102&vd_source=2286e10835cc4cd375b26d18d9e6fa7e
+// 你的任务是设计一个有固定加热半径的供暖器向所有房屋供暖。
+// 在加热器的加热半径范围内的每个房屋都可以获得供暖。
+// 现在，给出位于一条水平线上的房屋 houses 和供暖器 heaters 的位置，
+// 请你找出并返回可以覆盖所有房屋的最小加热半径。
+// 说明：所有供暖器都遵循你的半径标准，加热的半径都一样。
+// 测试链接 :https://leetcode.cn/problems/heaters
 class Heaters {
 public:
 

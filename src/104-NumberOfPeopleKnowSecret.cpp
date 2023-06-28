0465d060285c24d0c6a95b14a58b3b5d3a5e3030
韵 <98337638+CQJTU-Linzd@users.noreply.github.com>
2023-06-28 21:00:46 +0800

    Update 104-NumberOfPeopleKnowSecret.cpp

--- src/104-NumberOfPeopleKnowSecret.cpp
+++ src/104-NumberOfPeopleKnowSecret.cpp
@@ -1,5 +1,13 @@
 // 104
 // https://www.bilibili.com/video/BV1DT411s7hp?p=104&vd_source=2286e10835cc4cd375b26d18d9e6fa7e
+// 在第 1 天，有一个人发现了一个秘密。
+// 给你一个整数 delay ，表示每个人会在发现秘密后的 delay 天之后，
+// 每天 给一个新的人 分享 秘密。
+// 同时给你一个整数 forget ，表示每个人在发现秘密 forget 天之后会 忘记 这个秘密。
+// 一个人 不能 在忘记秘密那一天及之后的日子里分享秘密。
+// 给你一个整数 n ，请你返回在第 n 天结束时，知道秘密的人数。
+// 由于答案可能会很大，请你将结果对 109 + 7 取余 后返回。
+// 测试链接 : https://leetcode.cn/problems/number-of-people-aware-of-a-secret/
 class NumberOfPeopleKnowSecret {
 public:
 

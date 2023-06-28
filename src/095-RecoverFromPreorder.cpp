e62689d90eed02b00a8a211d0fc97141225f0d1a
韵 <98337638+CQJTU-Linzd@users.noreply.github.com>
2023-06-28 21:57:01 +0800

    Update 095-RecoverFromPreorder.cpp

--- src/095-RecoverFromPreorder.cpp
+++ src/095-RecoverFromPreorder.cpp
@@ -1,5 +1,12 @@
 // 95
 // https://www.bilibili.com/video/BV1DT411s7hp?p=95&vd_source=2286e10835cc4cd375b26d18d9e6fa7e
+// 我们从二叉树的根节点 root 开始进行深度优先搜索。
+// 在遍历中的每个节点处，我们输出 D 条短划线（其中 D 是该节点的深度）
+// 然后输出该节点的值。（如果节点的深度为 D，则其直接子节点的深度为 D + 1
+// 根节点的深度为 0
+// 如果节点只有一个子节点，那么保证该子节点为左子节点
+// 给出遍历输出 S，还原树并返回其根节点 root。
+// 测试链接 : https://leetcode.cn/problems/recover-a-tree-from-preorder-traversal/
 class RecoverFromPreorder {
 public:
 

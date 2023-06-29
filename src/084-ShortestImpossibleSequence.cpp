daab9087b93570a508863ed90c8de999fcff62ac
韵 <98337638+CQJTU-Linzd@users.noreply.github.com>
2023-06-29 19:47:08 +0800

    Update 084-ShortestImpossibleSequence.cpp

--- src/084-ShortestImpossibleSequence.cpp
+++ src/084-ShortestImpossibleSequence.cpp
@@ -1,5 +1,12 @@
 // 84
 // https://www.bilibili.com/video/BV1DT411s7hp?p=87&vd_source=2286e10835cc4cd375b26d18d9e6fa7e
+// 给你一个长度为 n 的整数数组 rolls 和一个整数 k 。
+// 你扔一个 k 面的骰子 n 次，骰子的每个面分别是 1 到 k ，
+// 其中第 i 次扔得到的数字是 rolls[i] 。
+// 请你返回 无法 从 rolls 中得到的 最短 骰子子序列的长度。
+// 扔一个 k 面的骰子 len 次得到的是一个长度为 len 的 骰子子序列 。
+// 注意 ，子序列只需要保持在原数组中的顺序，不需要连续。
+// 测试链接 : https://leetcode.cn/problems/shortest-impossible-sequence-of-rolls/
 class ShortestImpossibleSequence {
 public:
 
